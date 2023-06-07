4f5eb3d8ce51e75ad91c54b811b09fe783622205
ArturIlchenko <artur.v.ilchenko@yandex.ru>
2023-06-07 23:34:48 +0500

    finished carousel on the tini-slider

--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,25 @@ const slider = tns({
 	container: '.carousel__inner',
 	items: 1,
 	slideBy: 1,
-	autoplay: false,
+	autoplay: true,
+	autoplayButton: false,
+	autoplayButtonOutput: false,
 	controls: false,
-	nav: false
+	nav: true,
+	navPosition: 'bottom',
+	speed: 1500,
+	responsive: {
+		991: {
+			autoplay: false,
+			nav: false
+		}
+	}
 });
 
-document.querySelector('.carousel__btn_prev').addEventListener ('click', function () {
+document.querySelector('.carousel__btn_prev').addEventListener('click', function () {
 	slider.goTo('prev');
- });
+});
 
- document.querySelector('.carousel__btn_next').addEventListener ('click', function () {
+document.querySelector('.carousel__btn_next').addEventListener('click', function () {
 	slider.goTo('next');
- });
+});
