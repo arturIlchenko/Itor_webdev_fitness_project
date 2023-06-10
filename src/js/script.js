94c79f9efd33e88a37f6f49df735995a6785bba1
ArturIlchenko <artur.v.ilchenko@yandex.ru>
2023-06-11 01:05:44 +0500

    added 4 block

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,22 @@ document.querySelector('.carousel__btn_prev').addEventListener('click', function
 document.querySelector('.carousel__btn_next').addEventListener('click', function () {
 	slider.goTo('next');
 });
+
+$(document).ready(function () {
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('section.catalog').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+	function togglesSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+			})
+		})
+	};
+	togglesSlide('.catalog-item__link');
+	togglesSlide('.catalog-item__back');
+})
