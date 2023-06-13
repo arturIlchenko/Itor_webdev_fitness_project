ef86cd6a551f9fecf0d7f6660b0cc7477cf8c3b5
ArturIlchenko <artur.v.ilchenko@yandex.ru>
2023-06-13 20:39:40 +0500

    added button gotop

--- src/js/script.js
+++ src/js/script.js
@@ -101,4 +101,34 @@ $(document).ready(function () {
 			$('form').trigger('reset');
 		});
 	});
+	// скролинг и pageup
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a").on('click', function (event) {
+
+		// Убедитесь, что это. Hash имеет значение перед переоценкой поведения по умолчанию
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			const hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 1000, function () {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
 })
